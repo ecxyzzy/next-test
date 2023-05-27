556279c7744b9503b001fcb9b2462d7d3ec9b014
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2023-05-27 11:50:27 -0700

    feat: add docs redirect & hello api route

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "https://docs.api-next.peterportal.org/",
+        permanent: true,
+        basePath: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/:slug*",
+        destination: "/api/:slug*",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
